44a1b17a255a17d28cfd48a6e5d3fdb310f49508
Caerwyn13 <caerwyn13@hotmail.com>
2026-07-05 02:27:19 +0100

    Added TODOs
    Signed-off-by: Caerwyn13 <caerwyn13@hotmail.com>

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@
 using namespace std;
 
 int main() {
+    //TODO: Create selection menu
+    //TODO: Allow for several commands (view tasks, add task) to be ran in one go
+    //TODO: Add more commands (delete task, order tasks (in view tasks menu), change task metadata, etc.)
     const string taskFilePath = "tasks.json";
     vector<Task> tasks = readTasksFromFile(taskFilePath);
 
